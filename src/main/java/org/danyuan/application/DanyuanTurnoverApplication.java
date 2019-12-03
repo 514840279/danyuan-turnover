85a2d4c1c24406b5665b5c287cd7fcb7297e9068
王滕辉 <514840279@qq.com>
2019-12-03 08:51:06 +0800

    first commit

---
+++ src/main/java/org/danyuan/application/DanyuanTurnoverApplication.java
@@ -0,0 +1,13 @@
+package org.danyuan.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanyuanTurnoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanyuanTurnoverApplication.class, args);
+	}
+
+}
